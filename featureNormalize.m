8b0666409d30b3a6e53a740b6b6cbfd9f60305bf
kspavankrishna <58327514+kspavankrishna@users.noreply.github.com>
2020-06-23 11:58:11 +0530

    5

--- featureNormalize.m
+++ featureNormalize.m
@@ -5,35 +5,11 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
 
-% You need to set these values correctly
-X_norm = X;
 mu = mean(X);
-sigma = std(X);
-X = (X - mu)./sigma;
-X_norm = X;
+X_norm = bsxfun(@minus, X, mu);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
-
-
-
-
-
-
+sigma = std(X_norm);
+X_norm = bsxfun(@rdivide, X_norm, sigma);
 
 
 % ============================================================
