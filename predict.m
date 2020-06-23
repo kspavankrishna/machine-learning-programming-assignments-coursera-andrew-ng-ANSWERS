8598466e4d2dc0bc9ca4538f1476473c44408c23
kspavankrishna <58327514+kspavankrishna@users.noreply.github.com>
2020-06-23 11:40:36 +0530

    3

--- predict.m
+++ predict.m
@@ -1,20 +1,36 @@
-function p = predict(theta, X)
-%PREDICT Predict whether the label is 0 or 1 using learned logistic 
-%regression parameters theta
-%   p = PREDICT(theta, X) computes the predictions for X using a 
-%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
 
-m = size(X, 1); % Number of training examples
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly
-p = zeros(m, 1);
-p = round(sigmoid(X * theta));
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters. 
-%               You should set p to a vector of 0's and 1's
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X];
+
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+
+a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';
+hypo = sigmoid(z3);
+[probability indices] = max(hypo');
+p = indices';
 
 
 
